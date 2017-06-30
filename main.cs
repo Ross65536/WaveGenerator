ab2b8384a2d4c40a1f20a8311a3625ad43310b0e
Ros <liandtow@gmail.com>
2017-06-30 08:09:07 +0100

    working 1st

---
+++ main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAVgenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"..\..\..\music.wav";
+            WaveGenerator wave = new WaveGenerator(WaveExampleType.ExampleSineWave);
+            wave.Save(filePath);
+
+            //SoundPlayer player = new SoundPlayer(filePath);
+            //player.Play();
+        }
+    }
+}
